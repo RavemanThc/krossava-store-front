9edae6a7170c34e80bb7e24965f32962ee996231
RavemanThc <vlad.garkusha1999@gmail.com>
2026-06-21 21:36:16 +0300

    add order form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,69 +5,45 @@ import { fetchSneackers } from "../../services/FetchMovies";
 import PaginationButton from "./pagination/Pagination";
 
 import { useQuery } from "@tanstack/react-query";
-import type { Sneaker } from "../../types/sneaker";
-import { LoadMore } from "./pagination/LoadMore";
+import OrderForm from "./OrderForm/OrderForm";
 
 function App() {
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(0); // 0-based для API
-  const [accumulatedSneakers, setAccumulatedSneakers] = useState<Sneaker[]>([]);
-  const [isInfinite, setIsInfinite] = useState(false);
 
-  const { data, isLoading, isError, isFetching } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["sneakers", search, page], // Каждая страница — отдельный кэш
     queryFn: () => fetchSneackers({ search, page, perPage: 10 }),
-    // Проп placeholderData предотвращает "мигание" интерфейса при смене страницы
+    // Предотвращает "мигание" интерфейса и прыжки экрана при смене страницы
     placeholderData: (keepPreviousData) => keepPreviousData,
   });
 
-  // Следим за выгрузкой данных
   const totalPages = data?.totalPages ?? 0;
-  const hasNextPage = page + 1 < totalPages;
-
-  const currentProducts = data?.products || [];
+  const sneakersToShow = data?.products || [];
 
   const handleSearch = (query: string) => {
     setSearch(query);
-    setPage(0);
-    setAccumulatedSneakers([]);
+    setPage(0); // При новом поиске всегда сбрасываем на 1-ю страницу
   };
 
   const handlePageChange = (newPage: number) => {
-    setIsInfinite(false);
-    setPage(newPage - 1); // Переводим 1-based в 0-based
+    setPage(newPage - 1); // Переводим 1-based из компонента кнопки в 0-based для API
   };
-
-  const handleLoadMore = () => {
-    if (hasNextPage && !isFetching) {
-      setIsInfinite(true);
-      // Сначала сохраняем то, что уже было на экране, перед тем как переключить страницу
-      setAccumulatedSneakers((prev) => (prev.length ? prev : currentProducts));
-      setPage((prev) => prev + 1);
-    }
-  };
-
-  // Выбираем, что рендерить
-  const sneakersToShow = isInfinite
-    ? [...accumulatedSneakers, ...currentProducts]
-    : currentProducts;
 
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
-      <PaginationButton
-        totalPages={totalPages}
-        currentPage={page + 1}
-        onPageChange={handlePageChange}
-      />
       {isLoading && <p>Loading...</p>}
       {isError && <p>Error</p>}
       <SneakerGrid sneakers={sneakersToShow} onSelect={() => {}} />{" "}
-      <LoadMore
-        onClick={handleLoadMore}
-        hasNextPage={hasNextPage}
-        isFetchingNextPage={isFetching && isInfinite}
-      />
+      {totalPages > 1 && (
+        <PaginationButton
+          totalPages={totalPages}
+          currentPage={page + 1} // Переводим 0-based в 1-based для отображения
+          onPageChange={handlePageChange}
+        />
+      )}
+      <OrderForm />
     </>
   );
 }
