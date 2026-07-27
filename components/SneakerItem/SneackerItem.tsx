87907b65bc66dd3f5a851ffd62abdcc68a13faf4
RavemanThc <vlad.garkusha1999@gmail.com>
2026-07-27 23:05:14 +0300

    add articles

--- components/SneakerItem/SneackerItem.tsx
+++ components/SneakerItem/SneackerItem.tsx
@@ -16,29 +16,12 @@ const SneackerItem = ({ sneaker, onSelect }: SneackerCard) => {
 
   return (
     <li className={css.listitem}>
-      <Link href={`/sneakers/${sneaker.id}`} className={css.linkWrap}>
-        <div className={css.card}>
-          {/* 1. Левый блок: берем первые 5 элементов, рендерим их слева */}
-          <ul className={`${css.wrapsize} ${css.leftSide}`}>
-            {sneaker.sizes.slice(0, 5).map((sizeObj, index) => (
-              <li className={css.size} key={index}>
-                <button
-                  type="button"
-                  className={css.sizebutton}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    onSelect(sneaker, sizeObj.size);
-                  }}
-                >
-                  {sizeObj.size}
-                </button>
-              </li>
-            ))}
-          </ul>
-
-          {sneaker.sizes.length > 5 && (
-            <ul className={`${css.wrapsize} ${css.rightSide}`}>
-              {sneaker.sizes.slice(5).map((sizeObj, index) => (
+      <article className={css.linkWrap}>
+        <Link href={`/sneakers/${sneaker.id}`} className={css.linkWrap}>
+          <div className={css.card}>
+            {/* 1. Левый блок: берем первые 5 элементов, рендерим их слева */}
+            <ul className={`${css.wrapsize} ${css.leftSide}`}>
+              {sneaker.sizes.slice(0, 5).map((sizeObj, index) => (
                 <li className={css.size} key={index}>
                   <button
                     type="button"
@@ -53,45 +36,66 @@ const SneackerItem = ({ sneaker, onSelect }: SneackerCard) => {
                 </li>
               ))}
             </ul>
-          )}
-          <div className={css.imageBox}>
-            {imgLoading && <div className={css.imgSpace} />}
-            <Image
-              className={css.image}
-              src={sneaker.images?.[0] ?? "https://via.placeholder.com/500x750"}
-              onLoad={() => setImgLoading(false)}
-              onError={() => setImgLoading(true)}
-              style={{
-                opacity: imgLoading ? 0 : 1,
-                transition: "opacity 0.2s ease",
-              }}
-              alt={sneaker.title}
-              width={600}
-              height={600}
-              sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 363px"
-            />
-          </div>
-          <h2 className={css.title}>{sneaker.title}</h2>
-        </div>
-        <ul className={`${css.wrapsizemob} ${css.leftSide}`}>
-          {sneaker.sizes.map((sizeObj, index) => (
-            <li className={css.size} key={index}>
-              <button
-                type="button"
-                className={css.sizebutton}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  onSelect(sneaker, sizeObj.size);
+
+            {sneaker.sizes.length > 5 && (
+              <ul className={`${css.wrapsize} ${css.rightSide}`}>
+                {sneaker.sizes.slice(5).map((sizeObj, index) => (
+                  <li className={css.size} key={index}>
+                    <button
+                      type="button"
+                      className={css.sizebutton}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        onSelect(sneaker, sizeObj.size);
+                      }}
+                    >
+                      {sizeObj.size}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
+            <div className={css.imageBox}>
+              {imgLoading && <div className={css.imgSpace} />}
+              <Image
+                className={css.image}
+                src={
+                  sneaker.images?.[0] ?? "https://via.placeholder.com/500x750"
+                }
+                onLoad={() => setImgLoading(false)}
+                onError={() => setImgLoading(true)}
+                style={{
+                  opacity: imgLoading ? 0 : 1,
+                  transition: "opacity 0.2s ease",
                 }}
-              >
-                {sizeObj.size}
-              </button>
-            </li>
-          ))}
-        </ul>
-        <p className={css.price}>Ціна:{sneaker.price}</p>
-        <p className={css.barcode}>Артикль: {sneaker.barcode}</p>
-      </Link>
+                alt={sneaker.title}
+                width={600}
+                height={600}
+                sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 363px"
+              />
+            </div>
+            <h2 className={css.title}>{sneaker.title}</h2>
+          </div>
+          <ul className={`${css.wrapsizemob} ${css.leftSide}`}>
+            {sneaker.sizes.map((sizeObj, index) => (
+              <li className={css.size} key={index}>
+                <button
+                  type="button"
+                  className={css.sizebutton}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    onSelect(sneaker, sizeObj.size);
+                  }}
+                >
+                  {sizeObj.size}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <p className={css.price}>Ціна:{sneaker.price}</p>
+          <p className={css.barcode}>Артикль: {sneaker.barcode}</p>
+        </Link>
+      </article>
     </li>
   );
 };
