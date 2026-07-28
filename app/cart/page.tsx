336710fad04af1ecab54cacfcc2cbb33b3ba1652
RavemanThc <vlad.garkusha1999@gmail.com>
2026-07-29 00:27:55 +0300

    change modal menu color icon

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,10 +1,12 @@
 import CartClient from "@/components/Cart/CartClient";
 import css from "./Cart.module.css";
+import RecentlyViewed from "@/components/RecentlyViewed/RecentlyViewed";
 export default function CartPage() {
   return (
     <main>
       <h1 className={css.carttitle}>Кошик</h1>
       <CartClient />
+      <RecentlyViewed />
     </main>
   );
 }
