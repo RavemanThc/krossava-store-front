65b310ac82556170e059a9ba9d3c4c0dea015e72
RavemanThc <vlad.garkusha1999@gmail.com>
2026-07-28 21:55:51 +0300

    change  sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,20 +1,32 @@
-import { MetadataRoute } from "next";
+import { fetchSneackers } from "@/src/lib/api";
+import type { MetadataRoute } from "next";
 
-const SITE_URL = "https://krossava.com.ua";
+const SITE_URL = "https://krossava.com.ua/";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { products } = await fetchSneackers({
+    page: 1,
+    perPage: 5000,
+  });
+
+  const sitemapProducts = products.map((product) => ({
+    url: `${SITE_URL}/sneakers/${product.id}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly" as const,
+    priority: 0.8,
+  }));
+
   return [
     {
       url: SITE_URL,
       lastModified: new Date(),
-      changeFrequency: "daily",
       priority: 1,
     },
     {
       url: `${SITE_URL}/sneakers`,
       lastModified: new Date(),
-      changeFrequency: "daily",
       priority: 0.9,
     },
+    ...sitemapProducts,
   ];
 }
